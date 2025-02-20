37ad72efbe49afd42ef56c46316123f6afc17052
aadilsalimm <130083334+aadilsalimm@users.noreply.github.com>
2025-02-20 18:43:38 +0530

    Implemented packet capture as a background thread. Real-time status is displayed on the browser. SocketIO is added for real-time updating of the webpage.

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
+import threading
+from flask_socketio import SocketIO
+import webbrowser
 from prediction_module.make_prediction import load_model, predict
 from network_capture.capture import start_sniff
 
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 model = load_model()
+
+attack_status = {"status": "No attack detected", "details": ""}
 
 @app.route('/')
 def home():
-    input = start_sniff()
-    prediction = predict(model, input)
-    return str(prediction)
+    return render_template("sample.html")
 
 
+def packet_capture():
+    global attack_status
+    while True:
+        input = start_sniff()
+        predictions = predict(model, input)
+        print(predictions)
+        if 'DDos' or 'Dos' in predictions:
+            attack_status["status"] = "Attack detected!!!"
+            socketio.emit("attack_update",attack_status)
+        
 
 #app.debug = True
 if __name__ == '__main__':
-    app.run(debug=True)
+    threading.Thread(target=packet_capture, daemon=True).start()
+    webbrowser.open("http://127.0.0.1:5000")
+    socketio.run(app, debug=True)
