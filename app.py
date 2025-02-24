d389ceb4e26120d227d626c6c9ac8e6ccee67db3
aadilsalimm <130083334+aadilsalimm@users.noreply.github.com>
2025-02-24 23:40:22 +0530

    Updated the frontend page.

--- app.py
+++ app.py
@@ -11,37 +11,57 @@ app = Flask(__name__)
 socketio = SocketIO(app, async_mode='threading')
 model = load_model()
 
-attack_status = {"status": "No attack detected", "details": ""}
+attack_status = {"status": "No attack detected",
+                 "total": 0, "benign": 0, "dos": 0, "ddos": 0}
 server_started = False
 
 @app.route('/')
 def home():
-    return render_template("sample.html")
+    return render_template("home.html")
 
 
+total_captured = 0
+benign_count = 0
+dos_count = 0
+ddos_count = 0
 def packet_capture():
     global attack_status
+    global total_captured
+    global benign_count
+    global dos_count
+    global ddos_count
+
     while True:
         input = start_sniff()
         predictions = list(predict(model, input))
         print(predictions)
+        
+        total_captured += len(predictions)
+        dos_count += predictions.count('DoS')
+        ddos_count += predictions.count('DDoS')
+        benign_count += predictions.count('Benign')
+
+        attack_status["total"] = total_captured
+        attack_status["benign"] = benign_count
+        attack_status["dos"] = dos_count
+        attack_status["ddos"] = ddos_count
+
         if predictions.count("Benign") < len(predictions)/2:
             attack_status["status"] = "Attack detected!!!"
-            attack_status["details"] = f'Benign count : {predictions.count("Benign")}'
             socketio.emit("attack_update",attack_status)
         else:
             attack_status["status"] = "No attack detected"
-            attack_status["details"] = ""
             socketio.emit("attack_update",attack_status)
         
 
 def start_server():
     global server_started
     socketio.run(app, debug=False, use_reloader=False)
-    server_started = True
+    socketio.emit("attack_update", attack_status)
 
 
 if __name__ == '__main__':
+    server_started = True
     threading.Thread(target=start_server, daemon=True).start()
     threading.Thread(target=packet_capture, daemon=True).start()
     
