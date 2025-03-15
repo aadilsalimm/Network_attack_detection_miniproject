f3aa02b37b7ab57056a8ca41a29babffd3b1f7e9
aadilsalimm <aadilsalimm@gmail.com>
2025-03-16 00:51:28 +0530

    Fixed issue with iptable-persistent saving.

--- mitigation/mitigate.py
+++ mitigation/mitigate.py
@@ -3,9 +3,11 @@ import os
 def block_ip(mal_ips):
     for ip in mal_ips:
         os.system(f'sudo iptables -A INPUT -s {ip} -j DROP')
+        os.system('sudo netfilter-persistent save')
         print(f'{ip} blocked.')
 
 
 def unblock_ip(ip):
     os.system(f'sudo iptables -D INPUT -s {ip} -j DROP')
+    os.system('sudo netfilter-persistent save')
     print(f'{ip} unblocked.')        
